98988b4f1c5bd9c19a4919261f868865a099c8b5
RobCranfill <robcranfill@comcast.net>
2014-08-25 09:50:50 -0700

    Final version?
    Tidied up code, catch more errors, give proper HTTP status codes
    (right?).

--- fakeu.js
+++ fakeu.js
@@ -2,8 +2,9 @@
 	A simple GUI for heyu - Node.js version.
 
 	23/Aug/2014 - First js version, with no client screen size accomodations.
+	24/Aug/2014 - 2nd version - does do screen size accomodations (in the css, actually).
 	
-		Copyright (C) 2014 Rob Cranfill (robcranfill@gmail.com)
+  Copyright (C) 2014 Rob Cranfill (robcranfill@gmail.com)
 	
 		This program is free software; you can redistribute it and/or modify
 		it under the terms of the GNU General Public License as published by
@@ -21,19 +22,23 @@
     
 */
 
-var Exec				= require('child_process');
-var Fs				  = require("fs");
-var Http				= require("http");
-var Path				= require("path");
-var Querystring	= require("querystring");
-var Transform		= require('stream').Transform;
-var Url					= require("url");
+// Perhaps cuz I'm a Java programmer, I like to name these sorts of things - whether 
+// Classes - with an initial upper case letter.
+//
+var ChildProcess	= require('child_process');
+var Fs						= require("fs");
+var Http					= require("http");
+var Path					= require("path");
+var QueryString		= require("querystring");
+var Stream				= require('stream');
+var Url						= require("url");
 
-
+// Simple global vars - named with an underscore after.
+// 
 var requestCount_ = 0;
 var heyuCount_ = 0;
 
-// Content types map - the only things we'll serve
+// "Content types" map - the only things we'll serve. For now, just .css.
 //
 var contentTypes_ = {
 //	'.htm' : 'text/html',
@@ -45,31 +50,30 @@ var contentTypes_ = {
 
 
 /*
-	Handle:
+  This is where we handle each URL request.
 
-		Path													Action
-		--------------								--------------
-		/fakeu												Show main page
-		
-		/fakeu?unitCode=A1&action=on  Turn A1 on - invoke "heyu fon A1"
-																	Action is "on" or "off"
+    Path                            Action
+    --------------                  --------------
+    /fakeu                          Show main HTML page
 
+    /fakeu?unitCode=A1&action=on    Turn A1 on - invoke "heyu fon A1"
+                                    Action is "on" or "off"
 */
 function router(req, resp) {
 
-  console.log("-----------------------------------");
 	requestCount_++;
-
+	
 	var url = req.url;
   var parsedURL = Url.parse(url);
-  
+
+  console.log("-----------------------------------");
   console.log("Request #" + requestCount_ + " for path " + parsedURL.pathname + " received.");
-  console.log("Routing a request for " + parsedURL.pathname);
-	console.log('  search: ' + parsedURL.search);
-	console.log('   query: ' + parsedURL.query);
-	console.log('pathname: ' + parsedURL.pathname);
-	console.log('    path: ' + parsedURL.path);
-	console.log('    href: ' + parsedURL.href);
+//  console.log("Routing a request for " + parsedURL.pathname);
+//	console.log('  search: ' + parsedURL.search);
+//	console.log('   query: ' + parsedURL.query);
+//	console.log('pathname: ' + parsedURL.pathname);
+//	console.log('    path: ' + parsedURL.path);
+//	console.log('    href: ' + parsedURL.href);
 
 	if (parsedURL.path === "/fakeu") {
 
@@ -92,28 +96,27 @@ function router(req, resp) {
 
 		// Open the (mostly) static HTML file we will serve.
 		//
-		var fileStream = Fs.createReadStream("./fakeuMain.html");
+		var fileStream = Fs.createReadStream("fakeuMain.html");
 		fileStream.on('error', function(error) {
 			if (error.code === 'ENOENT') {
 				resp.statusCode = 404;
 				resp.end(Http.STATUS_CODES[404]);
-			  console.log("!Error 404!");
-				} 
+				console.log("!Error 404 on fakeuMain.html!");
+				console.log("!Error is: " + error);
+				}
 			else {
 				resp.statusCode = 500;
 				resp.end(Http.STATUS_CODES[500]);
-			  console.log("!Error 500!");
+			console.log("!Error 500 on fakeuMain.html!");
 				}
 			});
 		console.log("File loaded.");
 
-		resp.writeHead(200, {"Content-Type": "text/html"});
-
-		// Instead of simply doing
+		// Instead of simply piping the file to the response, like this:
 		//		fileStream.pipe(resp);
 		// we transform the stream, substituting the name of the CSS file to use.
 		//
-		var parser = new Transform();
+		var parser = new Stream.Transform();
 		parser._transform = function(data, encoding, done) {
 			var dataAsString = data.toString().replace("_CSS_FILE_NAME_", cssFileName);
 //			console.log("Transformed:\n-- in --\n" + data + "\n-- out --\n" + dataAsString);
@@ -123,22 +126,22 @@ function router(req, resp) {
 
 		// Pipe the streams
 		//
+		resp.writeHead(200, {"Content-Type": "text/html"});
 		fileStream.pipe(parser).pipe(resp);
 
-		console.log("Page served!");
-		
+		console.log("Page served");
 		return;
-		}
+		} // path '/fakeu'
 
 
 	// We are looking to handle a request like:
-	// 	http://host:8888/fakeu?unitCode=A1&action=on
+	//  http://host:8888/fakeu?unitCode=A1&action=on
 	//
 	if (parsedURL.query) {
 	
-		var parsedQuery = Querystring.parse(parsedURL.query);
-	  console.log("parsedQuery: " + JSON.stringify(parsedQuery));
-	  
+		var parsedQuery = QueryString.parse(parsedURL.query);
+		console.log("parsedQuery: " + JSON.stringify(parsedQuery));
+
 		var unitCode = parsedQuery["unitCode"];
 		var action   = parsedQuery["action"];
 		console.log("unitCode: " + unitCode + "; action: " + action);
@@ -147,18 +150,19 @@ function router(req, resp) {
 		// TODO: CHECK UNIT CODE
 		//
 		if (action !== "on" & action !== "off") {
-			console.log("!Bad action!");
-			handleBadRequest(resp);
+			console.log("!Bad action (" + action + ")!");
+			handleBadRequest(resp, 404);
 			return;
 			}
 
 		// Execute heyu!
+		// I am using a "Firecracker", so the heyu commands are actually "fon" and "foff".
 		//
 		heyuCount_++;
 		var heyuCommand = "/usr/local/bin/heyu f" + action + " " + unitCode;
 		console.log("heyu command #" + heyuCount_ + ": '" + heyuCommand + "'");
 
-		Exec.exec(heyuCommand, function (error, stdout, stderr) {
+		ChildProcess.exec(heyuCommand, function (error, stdout, stderr) {
 			if (error) {
 				console.log("Result from heyu.cmd: " + error);
 				console.log("stdout: '" + stdout + "'");
@@ -166,95 +170,96 @@ function router(req, resp) {
 				}
 			});
 
-		// Not sure what the heck is going on here.
 		// If we don't end the response, the browser isn't happy.
-		// Kinda makes sense.
+		// Doing this without sending any response content seems to keep 
+		// us on the same (main) page, which is good.
 		//
 		resp.end();
-
 		return;
-		} // parsedURLQuery
+		} // parsedURLQuery OK
 	
 
-	// At this point, it's not a request for the main page or for a 'heyu' command.
+	// At this point, it's not a request for the main page, or for a 'heyu' command.
 	// It may be a request for a supporting page, such as a .css file.
 	//
-  // Get the extension; is it a special file we'll handle?
+  // Get the extension; is it a type that we can/will handle?
   //
   var extension = Path.extname(url);
 	console.log("Checking extension for '" + parsedURL.path + "': '" + extension + "'");
 
-  // Read the extension against the content type map - default to plain text
+  // Read the extension against the content type map - default to plain text.
+  //
 	if (extension.length > 0) {
-	
-	  if (!contentTypes_[extension]) {
+		if (!contentTypes_[extension]) {
 			console.log("!Unknown content type! (" + extension + ")");
-			handleBadRequest(resp);
+			handleBadRequest(resp, 404);
 			return;
 			}
-	  var contentType = contentTypes_[extension];
-	  console.log("contentType OK: " + contentType);
+		var contentType = contentTypes_[extension];
+		console.log("contentType OK: " + contentType);
 
-	  // Serve the file
+		// Serve the file
 		//
 		var filename = Path.basename(parsedURL.path);
-	 	console.log("Serving file: " + filename);
+		console.log("Serving file: " + filename);
 		var fileStream = Fs.createReadStream(filename);
 		fileStream.on('error', function(error) {
 			if (error.code === 'ENOENT') {
 				resp.statusCode = 404;
-				resp.end(Http.STATUS_CODES[404]);
-			  console.log("!Error 404!");
+				resp.end(Http.STATUS_CODES[resp.statusCode]);
+				console.log("!Error 404 on '" + filename + "'!");
 				} 
 			else {
 				resp.statusCode = 500;
-				resp.end(Http.STATUS_CODES[500]);
-			  console.log("!Error 500!");
+				resp.end(Http.STATUS_CODES[resp.statusCode]);
+				console.log("!Error 500 on '" + filename + "'!");
 				}
 			});
 
 		resp.writeHead(200, {"Content-Type": contentType});
 		fileStream.pipe(resp);
 
-		console.log("File served!");
-
+		console.log("File served.");
 		return;
 		}
 
 
-	// Also not sure when/if/why this would happen.
-	// It doesn't seem to.
+	// Any other request comes here. Reply with a "400: Bad Request"
 	//
-	resp.end();
-	console.log(" *** FELL OUT OF END OF ROUTER *** ");
+	handleBadRequest(resp, 400);
+	console.log("!Fell out of end of router; replying with 400 error!");
 	}
 
 
 /*
+	Emit the indicated HTTP status code.
+*/
+function handleBadRequest(resp, code) {
+	resp.statusCode = code;
+	resp.end(Http.STATUS_CODES[code]);
+	}
+
+
+/*
+	Handle an HTTP request.
+*/
+function onRequest(request, response) {
+	if (request.method !== "GET") {
+		console.log("!Not a GET!");
+		handleBadRequest(response, 404); // *could* do a 405 if we then sent the 'Allow' header. meh.
+		return;
+		}
+	router(request, response);
+	console.log("Request routed");
+	}
+	
+	
+/*
+	Here we are now: the main code.
 	Start the 'router', handling requests.
 */
-function start(router) {
-
-  function onRequest(request, response) {
-  	if (request.method !== "GET") {
-			console.log("!Not a GET!");
-			handleBadRequest(response);
-			return;
-			}
-    router(request, response);
-    console.log("Request routed");
-	  }
-
-  Http.createServer(onRequest).listen(8888);
-  console.log("Server has started on port 8888.");
-	}
+Http.createServer(onRequest).listen(8888);
+console.log("Server has started on port 8888.");
 
 
-function handleBadRequest(resp) {
-	resp.statusCode = 404;
-	resp.end('404\n');
-	}
-
-
-start(router);
-
+// End of program.
